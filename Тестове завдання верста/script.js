bb486bc18ce42f94bbcd2341d67305be8ffae33a
RudVladyslav <83006496+RudVladyslav@users.noreply.github.com>
2021-05-01 16:27:29 +0300

    Add files via upload

---
+++ Тестове завдання верста/script.js
@@ -0,0 +1,9 @@
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
